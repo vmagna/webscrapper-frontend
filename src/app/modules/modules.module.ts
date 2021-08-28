4519b8b0545f49bad4d69aadc3479336e630b5e5
Vinicius <vh.magna@gmail.com>
2021-08-28 19:11:04 -0300

    init

--- src/app/modules/modules.module.ts
+++ src/app/modules/modules.module.ts
@@ -4,6 +4,7 @@ import {CommonModule} from '@angular/common';
 import {RouterModule} from '@angular/router';
 import {AdminLayoutModule} from './admin.layout/admin.layout.module';
 
+
 @NgModule({
   declarations: [
     ModulesComponent,
